6240d84f9f18afd8b2398af6e5ba8dba972485fc
Anna Karutina <anna.karutina@khk.ee>
2021-10-28 09:44:44 +0300

    Add task value to localStorage

--- app.js
+++ app.js
@@ -49,6 +49,21 @@ function addTask(event){
     // add li to ul
     const ul = document.querySelector('.collection');
     ul.appendChild(li);
+    // save task
+    addTaskToLocalStorage(task);
     taskInput.value = '';
     event.preventDefault();
+}
+
+function addTaskToLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    console.log(tasks);
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    console.log(tasks);
 }
